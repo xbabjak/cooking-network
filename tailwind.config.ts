6589c0696ad084d52ec68c390e047f9d5bacee3e
xbabjak <radovan.babjak@gmail.com>
2026-02-28 13:36:50 +0100

    fix: make dark mode richtext white

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
 export default {
+  darkMode: ["selector", '[data-mantine-color-scheme="dark"]'],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
