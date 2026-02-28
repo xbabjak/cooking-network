f0676fa3fcdd785a10fb0b0615bc080ad4d5cf84
xbabjak <radovan.babjak@gmail.com>
2026-02-28 13:32:20 +0100

    fix: allow richtext styles to be visible on the web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: [
@@ -27,5 +28,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 } satisfies Config;
